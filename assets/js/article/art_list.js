472ec8717c94848306e0fad1ad951c5820f7fb46
he <121223321@qq.com>
2020-06-28 14:14:20 +0800

    完成了修改名称与类别的弹窗效果

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,10 +1,10 @@
-$(function() {
+$(function () {
   var layer = layui.layer;
   var form = layui.form;
   var laypage = layui.laypage;
 
   // 定义美化时间的过滤器
-  template.defaults.imports.dataFormat = function(date) {
+  template.defaults.imports.dataFormat = function (date) {
     const dt = new Date(date);
 
     var y = dt.getFullYear();
@@ -41,12 +41,14 @@ $(function() {
       method: 'GET',
       url: '/my/article/list',
       data: q,
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('获取文章列表失败！');
         }
         // 使用模板引擎渲染页面的数据
         var htmlStr = template('tpl-table', res);
+        // console.log(res);
+
         $('tbody').html(htmlStr);
         // 调用渲染分页的方法
         renderPage(res.total);
@@ -59,7 +61,7 @@ $(function() {
     $.ajax({
       method: 'GET',
       url: '/my/article/cates',
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('获取分类数据失败！');
         }
@@ -73,7 +75,7 @@ $(function() {
   };
 
   // 为筛选表单绑定 submit 事件
-  $('#form-search').on('submit', function(e) {
+  $('#form-search').on('submit', function (e) {
     e.preventDefault();
     // 获取表单中选中项的值
     var cate_id = $('[name=cate_id]').val();
@@ -99,7 +101,7 @@ $(function() {
       // 触发 jump 回调的方式有两种：
       // 1. 点击页码的时候，会触发 jump 回调
       // 2. 只要调用了 laypage.render() 方法，就会触发 jump 回调
-      jump: function(obj, first) {
+      jump: function (obj, first) {
         // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
         // 如果 first 的值为 true，证明是方式2触发的
         // 否则就是方式1触发的
@@ -119,18 +121,21 @@ $(function() {
   }
 
   // 通过代理的形式，为删除按钮绑定点击事件处理函数
-  $('tbody').on('click', '.btn-delete', function() {
+  $('tbody').on('click', '.btn-delete', function () {
     // 获取删除按钮的个数
     var len = $('.btn-delete').length;
     console.log(len);
     // 获取到文章的 id
     var id = $(this).attr('data-id');
     // 询问用户是否要删除数据
-    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+    layer.confirm('确认删除?', {
+      icon: 3,
+      title: '提示'
+    }, function (index) {
       $.ajax({
         method: 'GET',
         url: '/my/article/delete/' + id,
-        success: function(res) {
+        success: function (res) {
           if (res.status !== 0) {
             return layer.msg('删除文章失败！');
           };
@@ -152,25 +157,74 @@ $(function() {
     });
   });
 
-    // 通过代理的形式，为 btn-edit 按钮绑定点击事件
-    var indexEdit = null
-    $('tbody').on('click', '.btn-edit', function() {
-      // 弹出一个修改文章标题与分类信息的层
-      indexEdit = layer.open({
-        type: 1,
-        area: ['500px', '250px'],
-        title: '修改文章标题与分类',
-        content: $('#dialog-edit').html()
-      });
-  
-      var id = $(this).attr('data-id');
-      // 发起请求获取对应分类的数据
+  // 修改文章分类与标题
+  // 通过代理的形式，为 btn-edit 按钮绑定点击事件
+  var indexEdit = null
+  $('tbody').on('click', '.btn-edit', function () {
+    initCate();
+    // 弹出一个修改文章标题与分类信息的层
+    indexEdit = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '修改文章标题与分类',
+      content: $('#dialog-edit').html()
+    });
+
+    var id = $(this).attr('data-id');
+    // 发起请求获取对应分类的数据
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/list',
+      data: q,
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('获取文章分类与列表失败！');
+        }
+        console.log(res);
+        form.val('form-edit', res.data);
+
+        // $('tbody').html(htmlStr);
+        // // 调用渲染分页的方法
+        // renderPage(res.total);
+
+      }
+    });
+
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/cates',
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('获取分类数据失败！');
+        }
+        // 调用模板引擎渲染分类的可选项
+        var htmlStr = template('tpl-cate', res);
+        $('[name=cate_id]').html(htmlStr);
+        // 通过 layui 重新渲染表单区域的UI结构
+        form.render();
+      }
+    });
+
+
+    // 通过代理的形式，为修改分类的表单绑定 submit 事件
+    $('body').on('submit', '#form-edit', function (e) {
+      e.preventDefault();
       $.ajax({
-        method: 'GET',
-        url: '/my/article/cates/' + id,
-        success: function(res) {
-          form.val('form-edit', res.data);
+        method: 'POST',
+        url: '/my/article/updatecate',
+        data: $(this).serialize(),
+        success: function (res) {
+          if (res.status !== 0) {
+            return layer.msg('更新分类数据失败！');
+          };
+          layer.msg('更新分类数据成功！');
+          layer.close(indexEdit);
+          initArtCateList();
         }
       });
     });
+
+
+
+  });
 });
